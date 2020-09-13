251b3fc49b6db06fa23775f8be87ac932572f4b1
Tyler lewis <32583706+foxface918@users.noreply.github.com>
2020-09-12 22:34:03 -0500

    Update TTSForm.Designer.cs

--- Speaker/c# version/TTSForm.Designer.cs
+++ Speaker/c# version/TTSForm.Designer.cs
@@ -1,162 +1,209 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Speech.Synthesis;
-using System.Net;
-using System.Linq.Expressions;
-using System.IO;
-using System.Threading;
-using System.Speech.Recognition;
-
-
 namespace TTS
 {
-    public partial class Form1 : Form
+    partial class TTSform
     {
-        public SpeechSynthesizer ss = new SpeechSynthesizer();
-        SpeechRecognitionEngine r = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
-        String data = "Nothing";
-        String data2 = "Nothing";
-        Thread thread;
-        String code = "null";
-        String blurt = "true";
-        String[] parse = {"null", "null", "null"};
-        public Form1()
-        {
-            InitializeComponent();
-            thread = new Thread(Get);
-            thread.Start();
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
         {
-            Choices commands = new Choices();
-            commands.Add(new string[] { "Speaker speak" });
-            GrammarBuilder build = new GrammarBuilder();
-            build.Append(commands);
-            Grammar grammar = new Grammar(build);
-            r.LoadGrammarAsync(grammar);
-            r.SetInputToDefaultAudioDevice();
-            r.SpeechRecognized += r_SpeechRecognitionEngine;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Mode = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Repeat last question";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Speed";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(0, 35);
+            this.trackBar1.Minimum = -10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(253, 45);
+            this.trackBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Volume";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(0, 99);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(253, 45);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Value = 100;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.comboBox1.Location = new System.Drawing.Point(0, 150);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(253, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "Male";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Voice gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Mode";
+            // 
+            // Mode
+            // 
+            this.Mode.FormattingEnabled = true;
+            this.Mode.Items.AddRange(new object[] {
+            "Blurt",
+            "Wait"});
+            this.Mode.Location = new System.Drawing.Point(0, 190);
+            this.Mode.Name = "Mode";
+            this.Mode.Size = new System.Drawing.Size(253, 21);
+            this.Mode.TabIndex = 10;
+            this.Mode.Text = "Blurt";
+            this.Mode.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 217);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(78, 20);
+            this.textBox1.TabIndex = 11;
             
-            
-        }
-
-        private void r_SpeechRecognitionEngine(object sender, SpeechRecognizedEventArgs e)
-        {
-            if (e.Result.Text == "Speaker speak") {
-                ss.SpeakAsync(data);
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            ss.Rate = trackBar1.Value;
-            ss.Volume = trackBar2.Value;
-                ss.SpeakAsync(data);
-        }
-
-
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            ss.SpeakAsyncCancelAll();
-            thread.Abort();
-            base.OnClosing(e);
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (comboBox1.Text == "Male")
-            {
-                ss.SelectVoiceByHints(VoiceGender.Male);
-            }
-            if (comboBox1.Text == "Female")
-            {
-                ss.SelectVoiceByHints(VoiceGender.Female);
-            }
-        }
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (Mode.Text == "Blurt")
-            {
-                this.blurt = "true";
-            }
-            if (Mode.Text == "Wait") {
-                this.blurt = "false";
-                r.RecognizeAsync(RecognizeMode.Multiple);
-               
-
-            }
-        }
-        
-        private void Get()
-        {
-            try
-            {
-                while (true)
-                {
-                    Thread.Sleep(500);
-                    if (this.blurt == "true")
-                    {
-                        WebRequest request = WebRequest.Create("webserver");
-                        WebResponse response = request.GetResponse();
-                        Stream datastream = response.GetResponseStream();
-                        StreamReader reader = new StreamReader(datastream);
-                        String data1 = reader.ReadToEnd();
-                        if (!data2.Equals(data1))
-                        {
-                            parse = data1.Split('|');
-                            data2 = data1;
-                            if (code == parse[2]) {
-                                data = (parse[0] + " said " + parse[1]);
-                                ss.SpeakAsync(data);
-                            }
-                        }
-
-                    }
-                    else if(this.blurt == "false")
-                    {
-                        WebRequest request = WebRequest.Create("webserver");
-                        WebResponse response = request.GetResponse();
-                        Stream datastream = response.GetResponseStream();
-                        StreamReader reader = new StreamReader(datastream);
-                        String data1 = reader.ReadToEnd();
-                        if (!data2.Equals(data1))
-                        {
-                            parse = data1.Split('|');
-                            data2 = data1;
-                            if (code == parse[2])
-                            {
-                                data = (parse[0] + " said " + parse[1]);
-                                Console.Beep();
-                            }
-                        }
-
-
-                    }
-                }
-            }
-            catch (WebException e)
-            {
-                MessageBox.Show("ERROR: " + e + " |  check internet connection :)");
-            }
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Class Code:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 26);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Set";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 282);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Mode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TTS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            String setcode = this.textBox1.Text;
-            code = setcode;
-            MessageBox.Show("Your class code was set to: " + code);
+        #endregion
 
-        }
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox Mode;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
     }
 }
+
